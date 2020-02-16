d482b33ed1486a84383fa815e8f32cdf8a021962
jamespan62537 <jamespan62537@gmail.com>
2020-02-16 10:10:21 +0800

    modified css&router-link to v-btn

--- src/components/pages/Home/template/HomePage.vue
+++ src/components/pages/Home/template/HomePage.vue
@@ -3,28 +3,28 @@
     <div class="home-page-container">
       <div class="welcome">
         <div class="title-bg flex">
-          <div class="title">
-            <router-link to="/shopping">WELCOME EUROPE</router-link>
+          <div>
+            <v-btn dark outlined large @click="shopping">WELCOME EUROPE</v-btn>
           </div>
         </div>
       </div>
       <div class="hot-product-container">
-        <ul class="hot-list flex">
+        <ul class="hot-list flex pl-0">
           <li class="list list-one">
             <div class="hover-effect flex">
-              <router-link to="/shopping">THE LONDON EYE</router-link>
+              <v-btn dark outlined large @click="shopping">THE LONDON EYE</v-btn>
             </div>
           </li>
-          <li class="list list-two">
+          <li class="list list-two pa-0">
             <div class="hover-effect flex">
-              <router-link to="/shopping">ITALY</router-link>
+              <v-btn dark outlined large @click="shopping">ITALY</v-btn>
             </div>
           </li>
         </ul>
       </div>
-      <div class="slo">
-        <div class="slo-effect flex">
-          <router-link to="/shopping" class="flex">ENJOY THE TRIP</router-link>
+      <div class="slo d-flex align-center justify-center">
+        <div>
+          <v-btn dark outlined large @click="shopping">ENJOY THE TRIP</v-btn>
         </div>
       </div>
       <div class="footer flex">
@@ -33,6 +33,21 @@
     </div>
   </div>
 </template>
+
+<script>
+import $ from "jquery"
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    shopping: function() {
+      var vm = this;
+      vm.$router.push("/shopping");
+    }
+  }
+}
+</script>
 
 <style scoped>
 @import "../../../../assets/css/_homepage.css";
