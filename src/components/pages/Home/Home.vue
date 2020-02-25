2991c8341b7fd663435c1e0282a0287f1c8f3b37
jamespan62537 <jamespan62537@gmail.com>
2020-02-25 09:53:47 +0800

    modified shopping markup

--- src/components/pages/Home/Home.vue
+++ src/components/pages/Home/Home.vue
@@ -1,31 +1,29 @@
 <template>
-  <div>
+  <div class="main">
     <Alert></Alert>
-    <v-content>
-      <v-container>
-        <v-toolbar
-          class="d-flex justify-start grey darken-1"
-          style="position: fixed; left: 0; right: 0; z-index: 5;"
-        >
-          <div class="mr-5">
-            <a class="text-muted" href="#" @click.prevent="changePage">
-              <img src="../../../assets/img/logo.png" alt />
-            </a>
-          </div>
-          <div>
-            <v-btn text dark @click="aboutUs">關於我們</v-btn>
-            <v-btn text dark @click="shopping">輕旅歐洲</v-btn>
-            <v-btn text dark @click="admin">ADMIN</v-btn>
-          </div>
-        </v-toolbar>
-      </v-container>
-      <v-container style="margin-top:64px;">
+    <div>
+      <v-toolbar
+        class="d-flex justify-start grey darken-1"
+        style="position: fixed; left: 0; right: 0; z-index: 5;"
+      >
+        <div class="mr-5">
+          <a class="text-muted" href="#" @click.prevent="changePage">
+            <img src="../../../assets/img/logo.png" alt />
+          </a>
+        </div>
+        <div>
+          <v-btn text dark @click="aboutUs">關於我們</v-btn>
+          <v-btn text dark @click="shopping">輕旅歐洲</v-btn>
+          <v-btn text dark @click="admin">ADMIN</v-btn>
+        </div>
+      </v-toolbar>
+      <div class="container-bg" style="width: 100%; !important">
         <div class="cover d-flex align-end">
           <p>TRAVEL NOW</p>
         </div>
         <router-view></router-view>
-      </v-container>
-    </v-content>
+      </div>
+    </div>
   </div>
 </template>
 
