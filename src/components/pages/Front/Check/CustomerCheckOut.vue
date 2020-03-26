a32ec51caae3ce145b56e7e39c927444e4922f1c
jamespan62537 <jamespan62537@gmail.com>
2020-03-26 14:39:25 +0800

    new build

--- src/components/pages/Front/Check/CustomerCheckOut.vue
+++ src/components/pages/Front/Check/CustomerCheckOut.vue
@@ -2,7 +2,7 @@
   <div>
     <div>
       <div class="cart-list-container mt-10" style="background: rgb(255, 255, 255);">
-        <form class="form-card" @submit.prevent="payOrder">
+        <form class="form-card">
           <table class="table">
             <thead>
               <th colspan="3" class="text-center">
@@ -17,7 +17,7 @@
               </tr>
               <tr v-for="item in order.products" :key="item.id">
                 <td class="align-middle">{{ item.product.title }}</td>
-                <td class="align-middle text-center">{{ item.qty }}/{{ item.product.unit }}</td>
+                <td class="align-middle text-center">{{ item.qty }}</td>
                 <td class="align-middle text-center">{{ item.final_total | currency}}</td>
               </tr>
             </tbody>
@@ -62,7 +62,7 @@
             </tbody>
           </table>
           <div class="text-right pay-btn" v-if="order.is_paid === false">
-            <button class="btn btn-danger mr-5">確認付款</button>
+            <button class="btn btn-danger mr-5" @click="payOrder">確認付款</button>
           </div>
           <div class="text-right pay-btn" v-if="order.is_paid === true">
             <router-link to="/shopping" class="btn btn-danger mr-5">回首頁</router-link>
@@ -87,30 +87,31 @@ export default {
     // 取得已加入購物車商品
     getOrder() {
       var vm = this;
+      vm.$store.dispatch("updateLoading", true);
       const api = `${process.env.VUE_APP_API}/order/${
         vm.orderId
       }`;
       this.$http.get(api).then(response => {
+        vm.$store.dispatch("updateLoading", false);
         vm.order = response.data.order;
-        console.log(response);
       });
     },
     // 付款
     payOrder() {
       var vm = this;
+      vm.$store.dispatch("updateLoading", true);
       const api = `${process.env.VUE_APP_API}/pay/${
         vm.orderId
       }`;
       this.$http.post(api).then(response => {
+        vm.$store.dispatch("updateLoading", false);
         vm.order = response.data.order;
-        console.log(response);
         vm.getOrder();
       });
     }
   },
   created() {
     this.orderId = this.$route.params.orderId;
-    //   console.log(this.order);
     this.getOrder();
   }
 };
